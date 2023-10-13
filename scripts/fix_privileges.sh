25266fde6f9817f7c1b361d912f11cfca7b26838
qngo4 <qngo2410hv@gmail.com>
2023-10-13 21:30:54 +0700

    qweas

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/AwsDemo/* jar
-chmod +x /home/ec2-user/AwsDemo/server_start.sh
-chmod +x /home/ec2-user/AwsDemo/server_stop.sh
+#chmod +x /home/ec2-user/AwsDemo/* jar
+#chmod +x /home/ec2-user/AwsDemo/server_start.sh
+#chmod +x /home/ec2-user/AwsDemo/server_stop.sh
+chmod +x ~/AwsDemo/gradle/wrapper/*jar
+chmod +x ~/AwsDemo/scripts/server_start.sh
+chmod +x ~/AwsDemo/scripts/server_stop.sh
